c80ddf240b79e09845e3b0ecaf7a97eb501fea5f
Travis Fischer <fisch0920@gmail.com>
2018-02-10 18:40:08 -0500

    add default export example

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Foo from './Foo'
 import Bar from './Bar'
 
 // export Foo and Bar as named exports
-export {
-  Foo,
-  Bar
-}
+export { Foo, Bar }
 
 // alternative, more concise syntax for named exports
 // export { default as Foo } from './Foo'
+
+// you could also export a default object containing your module's interface
+// note that if you export both named and default exports, rollup will complain.
+// if you know how to prevent this warning, please let me know :)
+// export default { Foo, Bar }
