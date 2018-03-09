2ba1523704a5a5ea86268831ef3ba619be22e12c
Okezie Okpara <okeziestanley@yahoo.com>
2018-03-09 14:46:15 +0100

    added npmingnore, upgraded to react 16, configured es6 lint

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 
-import ExampleComponent from 'react-modern-library-boilerplate'
+import ExampleComponent from 'react-modern-library-boilerplate';
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent text="Modern React component module"/>
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ export default class ExampleComponent extends Component {
 
     return (
       <div className={styles.test}>
-        Example Component: {text}
+        Example Common Net: {text}
       </div>
     )
   }
