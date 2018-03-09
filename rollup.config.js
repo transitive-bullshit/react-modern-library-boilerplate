201a8863aa821221126bd6b9200fa5698ab3a45b
Travis Fischer <fisch0920@gmail.com>
2018-03-09 17:39:50 -0500

    add rollup-plugin-url to support importing images

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
+import url from 'rollup-plugin-url'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 
@@ -25,6 +26,7 @@ export default {
     postcss({
       modules: true
     }),
+    url(),
     babel({
       exclude: 'node_modules/**'
     }),
