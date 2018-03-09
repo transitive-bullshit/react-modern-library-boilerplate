9e2e31b1754d4eedde792b3709193d8c02c1b7f2
Travis Fischer <fisch0920@gmail.com>
2018-03-09 17:44:23 -0500

    .

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-import url from 'rollup-plugin-url'
 import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
 
 import pkg from './package.json'
 
